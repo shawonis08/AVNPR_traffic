dc5f62a4852044e96aa9473cedefcf1fd5ef20ce
shawonis08 <shawon_math07@yahoo.com>
2018-03-07 17:08:17 +0600

    templates & static folder added with html check

--- views.py
+++ views.py
@@ -1,7 +1,12 @@
 from main import app
+from flask import render_template
+
+
 @app.route('/')
 def index():
     return '<h1>test</h1>'
 
 
-
+@app.route('/login')
+def login():
+    return render_template('login.html')
