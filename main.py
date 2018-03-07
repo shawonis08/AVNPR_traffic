b1e369043738e82aca82ac517f3b9c08c11d5c73
mhnavid <hasan.nvd@gmail.com>
2018-03-08 02:08:41 +0600

    from added

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+# from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-app.config.from_pyfile('config.py')
 
-db = SQLAlchemy(app)
+@app.route('/')
+def upload_file():
+    return render_template('login.html')
 
-from views import *
+# app.config.from_pyfile('config.py')
+#
+# db = SQLAlchemy(app)
+#
+# from views import *
 if __name__ == '__main__':
     app.run(debug=True)
